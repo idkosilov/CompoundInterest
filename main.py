0acb8d07bfc28a76fb14958c09efbcc5aae29afe
idkosilov <idkosilov@gmail.com>
2020-12-03 07:48:43 +0700

    works 3

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ Developer: Kosilov I.
 
 """
 
-from prettytable import PrettyTable
+
+def make_table(data):
+    table = "+-----------------------+-----------------------------------+" \
+             "-----------------------------------+-----------------------+\n"
+    table += "|\t" + '\t\t\t\t|\t'.join(["Месяц", "Основа инвестиций", "Сумма % за месяц", "Капитал"]) + "\t\t\t\t|\n"
+    table += "+-----------------------+-----------------------------------+" \
+             "-----------------------------------+-----------------------+\n"
+    for row in data:
+        table += f"|\t{row[0]}{(4*4+4-len(row[0]))*' '}|" \
+                 f"\t{row[1]} ${(4*4+14-len(row[1]))*' '}|" \
+                 f"\t{row[2]} ${(4*4+14-len(row[2]))*' '}|" \
+                 f"\t{row[3]} ${(4*4+2-len(row[3]))*' '}|\n"
+    table += "+-----------------------+-----------------------------------+" \
+             "-----------------------------------+-----------------------+\n"
+    return table
 
 
 def float_format(number):
@@ -49,13 +63,12 @@ def enter_data():
 def main():
     years, capital, interest_rate, investments = enter_data()
     for year in range(1, years + 1):
-        table = PrettyTable()
-        table.field_names = ["Месяц", "Основа инвестиций", "Сумма % за месяц", "Капитал"]
+        data = []
         for month in range(1, 13):
-            table.add_row([month, float_format(capital), float_format(capital * interest_rate / 100),
-                           float_format(capital * (1 + interest_rate / 100))])
+            data.append([str(month), float_format(capital), float_format(capital * interest_rate / 100),
+                         float_format(capital * (1 + interest_rate / 100))])
             capital = capital * (1 + interest_rate / 100) + investments
-        print(f"{year} год", table, sep="\n")
+        print(f"{year} год", make_table(data), sep="\n")
 
 
 if __name__ == '__main__':
